d1c5b4b9908ba939cb7a274a98aa75bf904e6ea4
nguyenvulong0509 <nguyenvulong0509@gmail.com>
2026-01-08 15:34:38 +0700

    int_model

---
+++ lessons/models/Intermediate/int_ecommerce__order_items_products.sql
@@ -0,0 +1,23 @@
+WITH products as (
+	SELECT
+		product_id,
+		department as product_department,
+		cost as product_cost,
+		retail_price as product_retail_price
+	FROM {{ ref('stg_ecommerce__products') }}
+)
+
+SELECT
+	oi.order_item_id,
+	oi.order_id,
+	oi.user_id,
+	oi.product_id,
+	oi.item_sales_price,
+	p.product_department,
+	p.product_cost,
+	p.product_retail_price,
+	oi.item_sales_price - p.product_cost AS item_profit,
+	p.product_retail_price - oi.item_sales_price AS item_discount
+FROM {{ ref ('stg_ecommerce__order_items')}} oi
+	LEFT JOIN products p
+		ON oi.product_id = p.product_id
