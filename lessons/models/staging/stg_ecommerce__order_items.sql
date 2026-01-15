f1551cd840995e9cab282570ec5fc36520cba3c0
nguyenvulong0509 <nguyenvulong0509@gmail.com>
2026-01-15 13:44:38 +0700

    model version testing

--- lessons/models/staging/stg_ecommerce__order_items.sql
+++ lessons/models/staging/stg_ecommerce__order_items.sql
@@ -9,12 +9,6 @@ SELECT
         order_id,
         user_id,
         product_id,
-        inventory_item_id,
-        status,
-        created_at,
-        shipped_at,
-        delivered_at,
-        returned_at,
         sale_price as item_sales_price
 
 FROM source
